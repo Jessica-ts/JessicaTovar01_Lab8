39362b055e0c333372542a9d3890086d7a7a5ca3
Jessica Tovar <jessica_tovar_s@hotmail.com>
2019-11-18 13:15:56 -0600

    Delete

--- public/js/script.js
+++ public/js/script.js
@@ -127,7 +127,7 @@ function Delete()
 			contentType: "application/json",
 			success: function(responseJSON)
 			{
-				$("#blogPosts").remove();
+				$("#blogPosts").reset();
 				BlogPosts();
 			},
 			error: function(err)
